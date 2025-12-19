904992949be28949012dc07ee352fe420070b84a
AntonioOrf <antonio.orfitelli1@gmail.coM>
2025-12-19 14:27:55 +0100

    .

--- frontend-catasto/src/pages/HomePage.jsx
+++ frontend-catasto/src/pages/HomePage.jsx
@@ -140,3 +140,12 @@ export default function HomePage() {
     </div>
   );
 }
+
+function LoadTheme () {
+  const theme = localStorage.getItem('theme');
+  if (theme) {
+    document.documentElement.classList.add(dark);
+  } else {
+    document.documentElement.classList.remove(dark);
+  }
+}
