d49e1f196c828ee609666cc221586c61ddb11e92
AntonioOrf <antonio.orfitelli1@gmail.coM>
2025-12-19 20:48:09 +0100

    dark m

--- frontend-catasto/src/pages/HomePage.jsx
+++ frontend-catasto/src/pages/HomePage.jsx
@@ -15,7 +15,9 @@ import { useCatastoSidebar } from "../hooks/useCatastoSidebar";
 
 document.documentElement.classList.toggle(
   "dark",
-  localStorage.theme === "dark"
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
 );
 
 export default function HomePage() {
