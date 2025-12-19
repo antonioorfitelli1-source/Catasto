aae343dd0eb4bf5e09d172ab782f63b1786d0108
AntonioOrf <antonio.orfitelli1@gmail.coM>
2025-12-19 19:08:38 +0100

    .

--- frontend-catasto/src/components/layout/Header.jsx
+++ frontend-catasto/src/components/layout/Header.jsx
@@ -33,7 +33,7 @@ export default function Header({ isSidebarOpen, setIsSidebarOpen, darkMode, togg
               onChange={() => toggleDarkMode(!darkMode)}
               className="sr-only peer" 
             />
-            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-indigo-900"></div>
+            <div className="peer h-5 w-9 rounded-full bg-gray-400 after:absolute after:top-[2px] after:left-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-indigo-900 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-200"></div>
             <span className="ml-2 text-[10px] md:text-xs text-skin-text-accent">Dark Mode</span>
           </label>
     </div>
