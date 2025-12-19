1ce5068d3fa0e54fa77d5274b6b71d7b7ed3d00e
AntonioOrf <antonio.orfitelli1@gmail.coM>
2025-12-19 15:02:17 +0100

    .

--- frontend-catasto/src/pages/HomePage.jsx
+++ frontend-catasto/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ import { useCatastoSidebar } from '../hooks/useCatastoSidebar';
 
 function LoadTheme () {
   const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
+  if (savedTheme === 'dark' || (!savedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
   } else {
     document.documentElement.classList.remove('dark');
