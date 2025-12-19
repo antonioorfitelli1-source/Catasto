59f69fe3ea93b4087d03002aae68feb302a63419
AntonioOrf <antonio.orfitelli1@gmail.coM>
2025-12-19 19:21:55 +0100

    .

--- frontend-catasto/src/components/layout/Header.jsx
+++ frontend-catasto/src/components/layout/Header.jsx
@@ -33,11 +33,21 @@ export default function Header({ isSidebarOpen, setIsSidebarOpen, darkMode, togg
               onChange={() => toggleDarkMode(!darkMode)}
               className="sr-only peer" 
             />
-            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-5 peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-indigo-900 shadow-inner"></div>
-            <span className="ml-2 text-[10px] md:text-xs text-skin-text-accent font-bold hidden sm:inline">Dark Mode</span>
-          </label>
+          <span className="slider">
+    <div className="star star_1"></div>
+    <div className="star star_2"></div>
+    <div className="star star_3"></div>
+    <svg viewBox="0 0 16 16" className="cloud_1 cloud">
+      <path
+        transform="matrix(.77976 0 0 .78395-299.99-418.63)"
+        fill="#fff"
+        d="m391.84 540.91c-.421-.329-.949-.524-1.523-.524-1.351 0-2.451 1.084-2.485 2.435-1.395.526-2.388 1.88-2.388 3.466 0 1.874 1.385 3.423 3.182 3.667v.034h12.73v-.006c1.775-.104 3.182-1.584 3.182-3.395 0-1.747-1.309-3.186-2.994-3.379.007-.106.011-.214.011-.322 0-2.707-2.271-4.901-5.072-4.901-2.073 0-3.856 1.202-4.643 2.925"
+      ></path>
+    </svg>
+  </span>
     </div>
       </div>   
+    </label>      
     </header>
   );
 }
