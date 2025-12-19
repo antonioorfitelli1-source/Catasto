e6603584230612d024631c41c057bbc76fb11412
AntonioOrf <antonio.orfitelli1@gmail.coM>
2025-12-19 14:21:45 +0100

    Revert "feat: Add pagination component and configure initial global styling with Tailwind CSS."
    This reverts commit 253927e4b6ffb365ec276f36690b7e52aeeddbe1.

--- frontend-catasto/src/components/catasto/Pagination.jsx
+++ frontend-catasto/src/components/catasto/Pagination.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
-import '../../App.css';
 
 export default function Pagination({ page, totalPages, loading, handlePageChange }) {
   if (totalPages <= 0) return null;
